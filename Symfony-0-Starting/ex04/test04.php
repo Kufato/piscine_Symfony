5c6372bb49f1bbf4ca70ac511275511f2f36ef87
kufato <axel.callet@hotmail.com>
2024-10-16 15:38:26 +0200

    add the dir Symfony-0-Starting and ex00, ex01, ex02, ex03, ex04 and ex05 are finished

---
+++ Symfony-0-Starting/ex04/test04.php
@@ -0,0 +1,11 @@
+<?php
+include('./capital_city_from.php');
+
+echo capital_city_from('Oregon');
+
+echo capital_city_from('Origan');
+
+echo capital_city_from('Alabama');
+
+echo capital_city_from('Colorado');
+?>
